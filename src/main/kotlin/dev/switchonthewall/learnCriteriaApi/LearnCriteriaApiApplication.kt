7ec6f3184f7054063c5600671a827b631e01779d
halka asahina <halka.rjch@gmail.com>
2021-02-16 17:12:19 +0900

    init

---
+++ src/main/kotlin/dev/switchonthewall/learnCriteriaApi/LearnCriteriaApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.switchonthewall.learnCriteriaApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnCriteriaApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearnCriteriaApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/switchonthewall/learnCriteriaApi/LearnCriteriaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.switchonthewall.learnCriteriaApi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearnCriteriaApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
