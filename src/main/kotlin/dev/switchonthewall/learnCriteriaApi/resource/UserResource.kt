fabaccb427fa2f508919ef60760ddd4683d29ff6
halka asahina <halka.rjch@gmail.com>
2021-02-28 16:47:17 +0900

    Refined search of status impl

--- src/main/kotlin/dev/switchonthewall/learnCriteriaApi/resource/UserResource.kt
+++ src/main/kotlin/dev/switchonthewall/learnCriteriaApi/resource/UserResource.kt
@@ -3,14 +3,15 @@ package dev.switchonthewall.learnCriteriaApi.resource
 import dev.switchonthewall.learnCriteriaApi.model.User
 import dev.switchonthewall.learnCriteriaApi.repository.UserRepository
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import javax.persistence.EntityManager
+import javax.persistence.criteria.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/users")
-class UserResource(private val userRepository: UserRepository){
+class UserResource(private val userRepository: UserRepository, private val entityManager: EntityManager){
     @GetMapping
     fun getAll() = userRepository.findAll()
 
@@ -39,5 +40,14 @@ class UserResource(private val userRepository: UserRepository){
     @DeleteMapping("/{id}")
     fun deleteUser(@PathVariable(value = "id") id: Long){
         return userRepository.deleteById(id)
+    }
+
+    @GetMapping("/status/{status}")
+    fun findByStatus(@PathVariable(value = "status") status: String): List<User> {
+        val criteriaBuilder: CriteriaBuilder = entityManager.criteriaBuilder
+        val criteriaQuery: CriteriaQuery<User> = criteriaBuilder.createQuery(User::class.java)
+        val root: Root<User> = criteriaQuery.from(User::class.java)
+        criteriaQuery.where(root.get<String>("status").`in`(status.split(",")))
+        return entityManager.createQuery(criteriaQuery).resultList
     }
 }
